70a561c3dc1e9483575a2bd70386558e13a089c2
zuwipertiwi <133567303+zuwipertiwi@users.noreply.github.com>
2023-12-01 12:44:52 +0700

    Update COD.java

--- COD.java
+++ COD.java
@@ -1,7 +1,8 @@
-public class COD extends Pembayaran {
-   @Override
+class COD extends Pembayaran {
+    @Override
     public void prosesPembayaran(double total) {
         System.out.println("Pembayaran sebesar Rp" + total + " dengan metode COD berhasil.");
         System.out.println("Silakan bayar tunai saat barang diterima.");
     }
 }
+
