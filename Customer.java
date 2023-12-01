ea8dbf5affe8ec9f03841017136acfa74ddfe503
zuwipertiwi <133567303+zuwipertiwi@users.noreply.github.com>
2023-12-01 11:03:57 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -2,11 +2,55 @@ import java.util.ArrayList;
 
 public class Customer extends Akun {
     public Keranjang keranjang;
-    public ArrayList<Invoice> invoiceSelesai;
+    public List<Transaksi> transaksiSelesai;
 
-    public Customer(String username, String password) {
+   public Customer(String username, String password) {
         super(username, password);
-        this.keranjang = new Keranjang();
-        this.invoiceSelesai = new ArrayList<>();
+        keranjang = new Keranjang();
+        transaksiSelesai = new ArrayList<>();
+    }
+
+    public List<Transaksi> getTransaksiSelesai() {
+        return transaksiSelesai;
+    }
+
+    public void lihatListBarang(ListBarang listBarang) {
+        System.out.println("List Barang yang Dijual:");
+        listBarang.displayBarang();
+    }
+
+    // Fungsi untuk melihat daftar barang terbaru setelah admin menambahkan barang
+    public void lihatDaftarBarangTerbaru(Admin admin, ListBarang listBarang) {
+        admin.lihatDaftarBarang(listBarang);
+    }
+
+    public void tambahKeKeranjang(Barang barang) {
+        keranjang.tambahBarang(barang);
+        System.out.println(barang.getNama() + " telah ditambahkan ke keranjang.");
+    }
+
+    public void checkout(Pembayaran metodePembayaran) {
+        List<Barang> barangDibeli = keranjang.getListBarang();
+        if (barangDibeli.isEmpty()) {
+            System.out.println("Keranjang kosong. Tidak dapat checkout.");
+        } else {
+            Transaksi transaksi = new Transaksi(new ArrayList<>(barangDibeli), metodePembayaran);
+            transaksiSelesai.add(transaksi);
+            keranjang.clearKeranjang();
+            System.out.println("Checkout berhasil. Terima kasih atas pembelian Anda!");
+            System.out.println("Metode Pembayaran: " + metodePembayaran.getClass().getSimpleName());
+        }
+    }
+
+    public void lihatHistoryBelanja() {
+        System.out.println("History Belanja:");
+        for (Transaksi transaksi : transaksiSelesai) {
+            System.out.println("Transaksi ID: " + transaksi.getId());
+            System.out.println("Barang yang dibeli:");
+            for (Barang barang : transaksi.getDaftarBarang()) {
+                System.out.println("  - " + barang.getNama());
+            }
+            System.out.println("---------------------------");
+        }
     }
 }
