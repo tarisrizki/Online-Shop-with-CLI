fe045b7ff37553e6d338aa1d7693bef25449696a
zuwipertiwi <133567303+zuwipertiwi@users.noreply.github.com>
2023-12-01 11:01:52 +0700

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,5 +1,42 @@
 public class Admin extends Akun {
+    private List<Customer> daftarCustomer;
+    
     public Admin(String username, String password) {
         super(username, password);
+         daftarCustomer = new ArrayList<>();
+    }
+
+    public void tambahCustomer(Customer customer) {
+        daftarCustomer.add(customer);
+    }
+
+    public List<Customer> getDaftarCustomer() {
+        return daftarCustomer;
+    }
+
+    public void lihatTransaksi() {
+        System.out.println("Daftar Transaksi:");
+        for (Customer customer : daftarCustomer) {
+            List<Transaksi> transaksiSelesai = customer.getTransaksiSelesai();
+            if (!transaksiSelesai.isEmpty()) {
+                System.out.println("Transaksi untuk Pelanggan " + customer.getUsername() + ":");
+                for (Transaksi transaksi : transaksiSelesai) {
+                    System.out.println("Transaksi ID: " + transaksi.getId());
+                    System.out.println("Barang yang dibeli:");
+                    for (Barang barang : transaksi.getDaftarBarang()) {
+                        System.out.println("  - " + barang.getNama());
+                    }
+                    System.out.println("Metode Pembayaran: " + transaksi.getMetodePembayaran().getClass().getSimpleName());
+                    System.out.println("---------------------------");
+                }
+            } else {
+                System.out.println("Belum ada transaksi untuk Pelanggan " + customer.getUsername());
+            }
+        }
+    }
+
+    public void lihatDaftarBarang(ListBarang listBarang) {
+        System.out.println("Daftar Barang Terbaru:");
+        listBarang.displayBarang();
     }
 }
