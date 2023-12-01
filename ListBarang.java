62d27567c0f5ad4d314306dc6fa9ccd0420644d1
zuwipertiwi <133567303+zuwipertiwi@users.noreply.github.com>
2023-12-01 12:50:44 +0700

    Update ListBarang.java

--- ListBarang.java
+++ ListBarang.java
@@ -1,14 +1,14 @@
 import java.util.ArrayList;
+import java.util.List;
 
-public class ListBarang {
-    public ArrayList<Barang> listBarang = new ArrayList<>();
+class ListBarang {
+    private List<Barang> listBarang = new ArrayList<>();
 
     public void addBarang(Barang barang) {
         listBarang.add(barang);
     }
 
     public void hapusBarang(String id) {
-        // Hapus barang dengan ID yang sesuai
         Barang barang = cariBarang(id);
         if (barang != null) {
             listBarang.remove(barang);
@@ -22,13 +22,12 @@ public class ListBarang {
         }
     }
 
-    public Barang cariBarang(String id) {
-        // Cari barang dengan ID yang sesuai
+    public Barang cariBarang(String kode) {
         for (Barang barang : listBarang) {
-            if (barang.getId().equals(id)) {
+            if (barang.getId().equals(kode)) {
                 return barang;
             }
         }
-        return null; // Return null jika barang tidak ditemukan
+        return null;
     }
 }
