5831eecda08d458a50b90e438e5423896ba5335f
zuwipertiwi <133567303+zuwipertiwi@users.noreply.github.com>
2023-12-01 12:46:33 +0700

    Delete CustomerDriver.java

--- CustomerDriver.java
+++
@@ -1,7 +0,0 @@
-public class CustomerDriver extends Driver {
-    public Customer akun;
-    public Transaksi transaksi;
-    public ListBarang barang;
-
-    // Tambahkan fungsi atau menu customer jika diperlukan
-}
