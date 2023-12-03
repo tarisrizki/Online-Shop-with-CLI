803c47a5641a0560f973a2164583c851a100ed30
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:35:14 +0700

    Update Transaksi.java
    Melakukan penyesuaian dan menambahkan documentasi

--- Transaksi.java
+++ Transaksi.java
@@ -1,3 +1,8 @@
+/**
+ * Kelas ini merepresentasikan transaksi yang terjadi setelah proses checkout. Setiap transaksi memiliki ID unik.
+ * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
+ * @version 04/12/2023
+ */
 import java.util.List;
 
 class Transaksi {
@@ -6,12 +11,14 @@ class Transaksi {
     private List<Barang> daftarBarang;
     private Pembayaran metodePembayaran;
 
+    //constructor
     public Transaksi(List<Barang> daftarBarang, Pembayaran metodePembayaran) {
         this.id = counter++;
         this.daftarBarang = daftarBarang;
         this.metodePembayaran = metodePembayaran;
     }
 
+    //getter
     public int getId() {
         return id;
     }
