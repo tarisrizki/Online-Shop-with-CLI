4f8fe2c23ad74ac0f0aa18c5461ccd38aa7a9ba8
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:34:17 +0700

    Update Pembayaran.java
    Melakukan penyesuaian dan menambahkan documentasi

--- Pembayaran.java
+++ Pembayaran.java
@@ -1,3 +1,8 @@
+/**
+ * Kelas abstrak ini merepresentasikan metode pembayaran dan memiliki metode abstrak 
+ * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
+ * @version 04/12/2023
+ */
 abstract class Pembayaran {
     public abstract void prosesPembayaran(double total);
 }
