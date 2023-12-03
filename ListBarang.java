b1b5612b955f148da94b80f98a33f6629c327e5a
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:33:12 +0700

    Update ListBarang.java
    Melakukan penyesuaian dan menambahkan documentasi

--- ListBarang.java
+++ ListBarang.java
@@ -1,13 +1,20 @@
+/**
+ * Kelas ini merepresentasikan daftar barang yang tersedia di toko online.
+ * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
+ * @version 04/12/2023
+ */
 import java.util.ArrayList;
 import java.util.List;
 
 class ListBarang {
     private List<Barang> listBarang = new ArrayList<>();
 
+    //constructor
     public void addBarang(Barang barang) {
         listBarang.add(barang);
     }
 
+    //methode untuk hapus barang
     public void hapusBarang(String id) {
         Barang barang = cariBarang(id);
         if (barang != null) {
@@ -15,6 +22,7 @@ class ListBarang {
         }
     }
 
+    //methode untuk menampilkan barang yang tersedia
     public void displayBarang() {
         System.out.println("Daftar Barang yang Tersedia:");
         for (Barang barang : listBarang) {
@@ -22,6 +30,7 @@ class ListBarang {
         }
     }
 
+    //methode untuk cari barang
     public Barang cariBarang(String kode) {
         for (Barang barang : listBarang) {
             if (barang.getId().equals(kode)) {
