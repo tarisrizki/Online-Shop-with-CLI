e1711407d671a9847bbbc0d664838164ca167dd6
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:34:41 +0700

    Update QRIS.java
    Melakukan penyesuaian dan menambahkan documentasi

--- QRIS.java
+++ QRIS.java
@@ -1,3 +1,8 @@
+/**
+ * Kelas ini mewarisi dari kelas Pembayaran dan merepresentasikan metode pembayaran dengan QRIS.
+ * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
+ * @version 04/12/2023
+ */
 class QRIS extends Pembayaran {
     private String qrCode;
 
