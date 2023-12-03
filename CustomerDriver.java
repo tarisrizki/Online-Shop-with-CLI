c79bb7dcc56f8d516b3dffc06bdf2f7ce4416dc3
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:30:29 +0700

    Update CustomerDriver.java
    Melakukan penyesuaian dan menambahkan documentasi

--- CustomerDriver.java
+++ CustomerDriver.java
@@ -1,61 +1,69 @@
-import java.util.ArrayList;
-import java.util.List;
+/**
+ * Kelas ini mengelola interaksi pelanggan dengan sistem toko online. Pelanggan dapat melihat daftar barang, menambahkan barang ke keranjang, checkout, dan melihat riwayat belanja.
+ * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
+ * @version 04/12/2023
+ */
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CustomerDriver extends Driver {
     public Customer akun;
-     public List<Transaksi> riwayatBelanja;
+    public Transaksi transaksi;
+    public ListBarang barang;
 
-     public CustomerDriver() {
-        super(); // panggil constructor Driver
-        this.riwayatBelanja = new ArrayList<>();
-    }
-
+    //methode untuk menampilkan menu pelanggan
     public void menuPelanggan(Customer pelanggan, ListBarang listBarang, Scanner scanner) {
         int choice;
         do {
-            System.out.println("Menu Pelanggan:");
-            System.out.println("1. Lihat List Barang");
-            System.out.println("2. Tambah ke Keranjang");
-            System.out.println("3. Checkout");
-            System.out.println("4. Lihat History Belanja");
-            System.out.println("5. Keluar");
+            try { //exeption handling
+                System.out.println("Menu Pelanggan:");
+                System.out.println("1. Lihat List Barang");
+                System.out.println("2. Tambah ke Keranjang");
+                System.out.println("3. Checkout");
+                System.out.println("4. Lihat History Belanja");
+                System.out.println("5. Keluar");
 
-            System.out.print("Pilih: ");
-            choice = scanner.nextInt();
-            scanner.nextLine(); // Membuang karakter newline setelah nextInt
-            System.out.println();
+                System.out.print("Pilih: ");
+                choice = scanner.nextInt();
+                scanner.nextLine(); // Membuang karakter newline setelah nextInt
+                System.out.println();
 
-            switch (choice) {
-                case 1:
-                    pelanggan.lihatListBarang(listBarang);
-                    break;
-                case 2:
-                    System.out.print("Masukkan ID Barang yang ingin ditambahkan ke keranjang: ");
-                    String idBarang = scanner.nextLine();
-                    Barang barangDitambahkan = listBarang.cariBarang(idBarang);
-                    if (barangDitambahkan != null) {
-                        pelanggan.tambahKeKeranjang(barangDitambahkan);
-                    } else {
-                        System.out.println("Barang tidak ditemukan.");
-                    }
-                    break;
-                case 3:
-                    // Checkout dengan memilih metode pembayaran
-                    pilihMetodePembayaran(pelanggan, scanner);
-                    break;
-                case 4:
-                    pelanggan.lihatHistoryBelanja();
-                    break;
-                case 5:
-                    System.out.println("Keluar dari Menu Pelanggan");
-                    break;
-                default:
-                    System.out.println("Pilihan tidak valid. Coba lagi.");
+                switch (choice) {
+                    case 1:
+                        pelanggan.lihatListBarang(listBarang);
+                        break;
+                    case 2:
+                        System.out.print("Masukkan ID Barang yang ingin ditambahkan ke keranjang: ");
+                        String idBarang = scanner.nextLine();
+                        Barang barangDitambahkan = listBarang.cariBarang(idBarang);
+                        if (barangDitambahkan != null) {
+                            pelanggan.tambahKeKeranjang(barangDitambahkan);
+                        } else {
+                            System.out.println("Barang tidak ditemukan.");
+                        }
+                        break;
+                    case 3:
+                        // Checkout dengan memilih metode pembayaran
+                        pilihMetodePembayaran(pelanggan, scanner);
+                        break;
+                    case 4:
+                        pelanggan.lihatHistoryBelanja();
+                        break;
+                    case 5:
+                        System.out.println("Keluar dari Menu Pelanggan");
+                        break;
+                    default:
+                        System.out.println("Pilihan tidak valid. Coba lagi.");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Masukkan pilihan dengan benar. Coba lagi.");
+                scanner.nextLine(); // clear the buffer
+                choice = 0; // reset the choice to avoid an infinite loop
             }
         } while (choice != 5);
     }
 
+    //methode untuk menampilkan metode pembayaran
     public void pilihMetodePembayaran(Customer pelanggan, Scanner scanner) {
         System.out.println("Pilih Metode Pembayaran:");
         System.out.println("1. QRIS Payment");
