1d3795ca2f12718b8b6537665025d40fb9f9afa6
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:29:40 +0700

    Update COD.java
    Melakukan penyesuaian dan menambahkan documentasi

--- COD.java
+++ COD.java
@@ -1,4 +1,10 @@
+/**
+ * Kelas ini mewarisi dari kelas Pembayaran dan merepresentasikan metode pembayaran Cash On Delivery (COD).
+ * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
+ * @version 04/12/2023
+ */
 class COD extends Pembayaran {
+    //methode yang di override dari class Pembayaran
     @Override
     public void prosesPembayaran(double total) {
         System.out.println("Pembayaran sebesar Rp" + total + " dengan metode COD berhasil.");
