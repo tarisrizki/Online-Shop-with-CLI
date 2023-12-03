f2eacd456a1ff03b5b720c6641f262eaa119de57
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:27:55 +0700

    Update Bank.java
    Melakukan penyesuaian dan menambahkan documentasi

--- Bank.java
+++ Bank.java
@@ -1,10 +1,17 @@
+/**
+ * Kelas ini mewarisi dari kelas Pembayaran dan merepresentasikan metode pembayaran melalui transfer bank.
+ * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
+ * @version 04/12/2023
+ */
 class Bank extends Pembayaran {
     private String bank;
 
+    //constructor
     public Bank(String bank) {
         this.bank = bank;
     }
 
+    //methote yang dioverride dari class Pembayaran
     @Override
     public void prosesPembayaran(double total) {
         System.out.println("Pembayaran sebesar Rp" + total + " dengan transfer bank berhasil.");
