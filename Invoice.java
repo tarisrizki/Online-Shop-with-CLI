15ecb2530d2bf0289e83e76cbe35cdc51527081c
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:32:26 +0700

    Update Invoice.java
    Melakukan penyesuaian dan menambahkan documentasi

--- Invoice.java
+++ Invoice.java
@@ -1,9 +1,15 @@
+/**
+ * Kelas ini merepresentasikan invoice yang dibuat setelah proses checkout. Setiap invoice memiliki ID unik.
+ * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
+ * @version 04/12/2023
+ */
 public class Invoice {
     private static int counter = 1; // Untuk meng-generate ID invoice secara otomatis
     private int id;
-    public Transaksi transaksi;
-    public Pembayaran pembayaran;
+    private Transaksi transaksi;
+    private Pembayaran pembayaran;
 
+    //constructor
     public Invoice(Transaksi transaksi, Pembayaran pembayaran) {
         this.id = counter++;
         this.transaksi = transaksi;
