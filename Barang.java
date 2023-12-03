25573f13721b611fd2689e427769d5202514f824
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:28:45 +0700

    Update Barang.java
    Melakukan penyesuaian dan menambahkan documentasi

--- Barang.java
+++ Barang.java
@@ -1,17 +1,4 @@
-class Barang {
-    private String id;
-    private String nama;
-    private double harga;
-
-    public Barang(String id, String nama, double harga) {
-        this.id = id;
-        this.nama = nama;
-        this.harga = harga;
-    }
-
-    public String getId() {
-        return id;
-    }/**
+/**
  * Kelas ini merupakan kelas untuk objek barang dan semua atributnya
  * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
  * @version 04/12/2023
@@ -54,33 +41,6 @@ class Barang {
     }
 
     //methode yang di override
-    @Override
-    public String toString() {
-        return "[" + id + "] " + nama + " - Rp" + harga;
-    }
-}
-
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getNama() {
-        return nama;
-    }
-
-    public void setNama(String nama) {
-        this.nama = nama;
-    }
-
-    public double getHarga() {
-        return harga;
-    }
-
-    public void setHarga(double harga) {
-        this.harga = harga;
-    }
-
     @Override
     public String toString() {
         return "[" + id + "] " + nama + " - Rp" + harga;
