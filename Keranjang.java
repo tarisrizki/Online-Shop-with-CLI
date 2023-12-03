fce1a43969b5da27a5312c0572d640680e09a5c6
NaufalAqil18 <130956814+NaufalAqil18@users.noreply.github.com>
2023-12-04 00:32:47 +0700

    Update Keranjang.java
    Melakukan penyesuaian dan menambahkan documentasi

--- Keranjang.java
+++ Keranjang.java
@@ -1,17 +1,25 @@
+/**
+ * Kelas ini merepresentasikan keranjang belanja pelanggan dengan daftar barang yang dipilih.
+ * @author Naufal Aqil, M. Taris Riski, Zuwi Pertiwi
+ * @version 04/12/2023
+ */
 import java.util.ArrayList;
 import java.util.List;
 
 class Keranjang {
     private List<Barang> daftarBarang = new ArrayList<>();
 
+    //methode manambah barang ke keranjang
     public void tambahBarang(Barang barang) {
         daftarBarang.add(barang);
     }
 
+    //methode untuk membersihkan barang ke keranjang
     public void clearKeranjang() {
         daftarBarang.clear();
     }
 
+    //getter
     public List<Barang> getListBarang() {
         return daftarBarang;
     }
